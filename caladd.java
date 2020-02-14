0bcd1a3e3073c9eb738214be11c381116af7cce2
k-puneeth <puneeth.sarma@iiitb.org>
2020-02-14 20:19:08 +0530

    Second commit

--- caladd.java
+++ caladd.java
@@ -5,26 +5,29 @@ public class caladd {
     float a,b,res;
     char choice,ch;
     Scanner scan = new Scanner(System.in);
-    do{
+    // do{
       System.out.print("1.ADD TWO NUMBERS\n");
       System.out.print("2.Exit\n\n");
       System.out.print("Enter Your Choice :");
-      choice = scan.next().charAt(0);
-      switch(choice){
-        case '1': System.out.print("Enter Two Numbers :");
-                  a = scan.nextFloat();
-                  b = scan.nextFloat();
+      // choice = scan.next().charAt(0);
+      // choice = 1;
+      // switch(choice){
+        // case '1': System.out.print("Enter Two Numbers :");
+                  // a = scan.nextFloat();
+                  // b = scan.nextFloat();
+                  a = 2;
+                  b = 3;
                   res = a+b;
                   System.out.print("Result="+res);
-                  break;
+                  // break;
 
-        case '2': System.exit(0);
-                  break;
+        // case '2': System.exit(0);
+                  // break;
 
-        case '3':System.out.print("INVALID CHOICE!!!");
-                  break;
-      }
-      System.out.print("\n-------------------------------\n");
-    }while(choice!=2);
+        // case '3':System.out.print("INVALID CHOICE!!!");
+                  // break;
+      // }
+      // System.out.print("\n-------------------------------\n");
+    // }while(choice!=2);
   }
 }
